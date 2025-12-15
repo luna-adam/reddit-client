edbab58d140cd2ebc93b33a822ed7334c6a38cf9
la-877 <lunaadam41@gmail.com>
2025-12-15 18:25:05 +1100

    queryselector check for what contents i call from the api

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
+const postTitle = document.querySelector('.post-title');
+const postAuthor = document.querySelector('.post-author');
+const postVotes = document.querySelector('.vote-count');
 fetchSubreddit();
 
 async function fetchSubreddit() {
@@ -10,13 +12,15 @@ async function fetchSubreddit() {
         const response = await fetch(proxyEndpoint);
 
        if (!response.ok) {
-            // If the proxy returns an error status (like 500)
             const errorData = await response.json();
             throw new Error(`Proxy error: ${errorData.error || response.statusText}`);
         }
 
         const data = await response.json();
-        console.log(data.data.children);
+        postTitle.textContent = data.data.children[0].data.title;
+        postAuthor.textContent = data.data.children[0].data.author;
+        postVotes.textContent = data.data.children[0].data.ups;
+        console.log(data.data.children[0].data.ups);
     }
 
     catch(error){
