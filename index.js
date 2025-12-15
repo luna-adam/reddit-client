85a1601e4961dcbab4ef3f8a32954c59fb6459af
la-877 <lunaadam41@gmail.com>
2025-12-15 20:46:59 +1100

    by making column position relative i was able to but the buttons div above the content of a subreddit by making it absolute

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const postTitle = document.querySelector('.post-title');
 const postAuthor = document.querySelector('.post-author');
 const postVotes = document.querySelector('.vote-count');
 
-const posts = document.querySelector('.subreddit-column');
+const subreddits = document.querySelector('.subreddits');
 
 let data;
 
@@ -22,12 +22,8 @@ async function fetchSubreddit() {
         }
 
         data = await response.json();
-        postTitle.textContent = data.data.children[0].data.title;
-        postAuthor.textContent = data.data.children[0].data.author;
-        postVotes.textContent = data.data.children[0].data.ups;
-        console.log(data.data.children[0].data.ups);
 
-        renderPosts(data.data.children);
+        renderSubreddit(data.data.children);
     }
 
     catch(error){
@@ -37,46 +33,73 @@ async function fetchSubreddit() {
 
 
 
-function renderPosts(array){
+
+function renderSubreddit(array){
+    const subredditColumn = document.createElement('div');
+    subredditColumn.className = 'subreddit-column';
+
+    const subredditHeader = document.createElement('div');
+    subredditHeader.className = 'subreddit-header';
+
+    const subredditTitle = document.createElement('h2');
+    subredditTitle.className = 'subreddit-title';
+    subredditTitle.textContent = array[0].data.subreddit_name_prefixed;
+
+    const moreIcon = document.createElement('img');
+    moreIcon.className = 'more-icon';
+    moreIcon.src = '../images/more_vert.png'
+
+    moreIcon.addEventListener('click', () => {
+
+        const subButtons = document.createElement('div');
+        subButtons.className = 'subreddit-buttons';
+        subredditColumn.appendChild(subButtons);
+        console.log('ran');
+        
+    })
+
+    subredditHeader.appendChild(subredditTitle);
+    subredditHeader.appendChild(moreIcon);
+    subredditColumn.appendChild(subredditHeader);
 
     array.forEach(postitem => {
         
-    const post = document.createElement('div');
-    post.className = 'post';
+        const post = document.createElement('div');
+        post.className = 'post';
 
-    const votes = document.createElement('div');
-    votes.className ='votes';
-    const upIcon = document.createElement('img');
-    upIcon.src = '../images/up_vote.png';
-    upIcon.className = 'vote-icon';
-    const voteCount = document.createElement('p');
-    voteCount.className = 'vote-count';
-    voteCount.textContent = postitem.data.ups;
+        const votes = document.createElement('div');
+        votes.className ='votes';
+        const upIcon = document.createElement('img');
+        upIcon.src = '../images/up_vote.png';
+        upIcon.className = 'vote-icon';
+        const voteCount = document.createElement('p');
+        voteCount.className = 'vote-count';
+        voteCount.textContent = postitem.data.ups;
 
-    votes.appendChild(upIcon);
-    votes.appendChild(voteCount);
-    post.appendChild(votes);
+        votes.appendChild(upIcon);
+        votes.appendChild(voteCount);
+        post.appendChild(votes);
 
-    const content = document.createElement('div');
-    content.className = 'post-content';
-    const postTitle = document.createElement('h3');
-    postTitle.className = 'post-title';
-    postTitle.textContent = postitem.data.title;
-    const postAuthor = document.createElement('p');
-    postAuthor.className = 'post-author';
-    postAuthor.textContent = postitem.data.author;
+        const content = document.createElement('div');
+        content.className = 'post-content';
+        const postTitle = document.createElement('h3');
+        postTitle.className = 'post-title';
+        postTitle.textContent = postitem.data.title;
+        const postAuthor = document.createElement('p');
+        postAuthor.className = 'post-author';
+        postAuthor.textContent = postitem.data.author;
 
-    content.appendChild(postTitle);
-    content.appendChild(postAuthor);
-    post.appendChild(content);
+        content.appendChild(postTitle);
+        content.appendChild(postAuthor);
+        post.appendChild(content);
 
-    posts.appendChild(post);
-
-
-
+        subredditColumn.appendChild(post);
 
     });
 
+   
 
+
+    subreddits.appendChild(subredditColumn);
 
 }
