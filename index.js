e3f7cd15afa5c63b3413ec8ef3948218843519f9
la-877 <lunaadam41@gmail.com>
2025-12-15 19:29:53 +1100

    seeing if i create a new post class

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const postTitle = document.querySelector('.post-title');
 const postAuthor = document.querySelector('.post-author');
 const postVotes = document.querySelector('.vote-count');
+
+const posts = document.querySelector('.subreddit-column');
+
+let data;
+
 fetchSubreddit();
 
 async function fetchSubreddit() {
@@ -16,11 +21,13 @@ async function fetchSubreddit() {
             throw new Error(`Proxy error: ${errorData.error || response.statusText}`);
         }
 
-        const data = await response.json();
+        data = await response.json();
         postTitle.textContent = data.data.children[0].data.title;
         postAuthor.textContent = data.data.children[0].data.author;
         postVotes.textContent = data.data.children[0].data.ups;
         console.log(data.data.children[0].data.ups);
+
+        renderPosts(data.data.children, 20);
     }
 
     catch(error){
@@ -28,3 +35,38 @@ async function fetchSubreddit() {
     }
 }
 
+
+
+function renderPosts(array, index){
+    const post = document.createElement('div');
+    post.className = 'post';
+
+    const votes = document.createElement('div');
+    votes.className ='votes';
+    const upIcon = document.createElement('img');
+    upIcon.src = '../images/up_vote.png';
+    upIcon.className = 'vote-icon';
+    const voteCount = document.createElement('p');
+    voteCount.className = 'vote-count';
+    voteCount.textContent = array[index].data.ups;
+
+    votes.appendChild(upIcon);
+    votes.appendChild(voteCount);
+    post.appendChild(votes);
+
+    const content = document.createElement('div');
+    content.className = 'post-content';
+    const postTitle = document.createElement('h3');
+    postTitle.className = 'post-title';
+    postTitle.textContent = array[index].data.title;
+    const postAuthor = document.createElement('p');
+    postAuthor.className = 'post-author';
+    postAuthor.textContent = array[index].data.author;
+
+    content.appendChild(postTitle);
+    content.appendChild(postAuthor);
+    post.appendChild(content);
+
+    posts.appendChild(post);
+
+}
